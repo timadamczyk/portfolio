b48b5444578033a3f5579a766c1fcd3e3b9e1a8b
tim <tim@tims-iMac.local>
2019-03-27 20:13:31 -0700

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,15 +7,12 @@ class About extends Component {
     return (
 <article class="cf ph3 ph5-ns pv5">
   <header class="fn fl-ns w-50-ns pr4-ns">
-    <h1 class="mb3 mt0 lh-title">quisque non tellus orci</h1>
-    <time class="f6 ttu tracked gray">feugiat in</time>
+    <h1 class="mb3 mt0 lh-title">About</h1>
+    {/* <time class="f6 ttu tracked gray">feugiat in</time> */}
   </header>
   <div class="fn fl-ns w-50-ns">
     <p class="lh-copy measure mt4 mt0-ns">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At ultrices mi tempus imperdiet nulla. Malesuada fames ac turpis egestas maecenas pharetra convallis. Integer malesuada nunc vel risus commodo viverra maecenas. Ut eu sem integer vitae justo. Vestibulum sed arcu non odio euismod lacinia. Eu consequat ac felis donec et odio pellentesque diam volutpat. Auctor urna nunc id cursus metus aliquam eleifend mi. Faucibus et molestie ac feugiat sed. Iaculis eu non diam phasellus. Eu consequat ac felis donec et odio. Diam phasellus vestibulum lorem sed. Amet consectetur adipiscing elit pellentesque. Quis auctor elit sed vulputate mi. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Eu sem integer vitae justo eget magna fermentum iaculis eu. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Facilisi morbi tempus iaculis urna id volutpat.
-    </p>
-    <p class="lh-copy measure"> 
-    Blandit turpis cursus in hac habitasse platea dictumst. Enim nulla aliquet porttitor lacus. Integer enim neque volutpat ac tincidunt vitae semper. Nam libero justo laoreet sit amet. Eros donec ac odio tempor orci dapibus ultrices. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet. A diam sollicitudin tempor id eu nisl nunc. Dolor sit amet consectetur adipiscing elit duis tristique. Tortor vitae purus faucibus ornare suspendisse. Proin nibh nisl condimentum id venenatis a condimentum.
+    Tim Adamczyk is an artist and web developer based in Phoenix, born and raised in Chicago. His work is interdisciplinary, conceptual, and often in conversation with design or real-world economies. Tim received his Bachelor of Fine Arts degree  in sculpture from the School of the Art Institute of Chicago in 2015 and since has been working in sectors of architecture, web development, and arts administration. This website currently serves as a portfolio for his recent web-based projects.
     </p>
   </div>
 </article>
